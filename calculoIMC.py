f8c836b4a7326d3fcf17ea563ee1a185700993de
Mateus Adrade <128805906+MateusAndrade071@users.noreply.github.com>
2024-02-05 11:04:51 -0300

    Update calculoIMC.py

--- calculoIMC.py
+++ calculoIMC.py
@@ -5,4 +5,14 @@ altura = float(input("Me diga sua altura: "))
 
 calculoIMC = peso/(altura*altura)
 
-print(nome,"você tem", round(calculoIMC,2),"de massa corporal")
+
+if calculoIMC < 18.5:
+    print(f"{nome} está abaixo do peso")
+elif calculoIMC > 18.5 and calculoIMC < 24.9:
+    print(f"{nome} está com peso normal")
+elif calculoIMC > 25 and calculoIMC < 29.9:
+    print(f"{nome} está acima do peso")
+elif calculoIMC > 30 and calculoIMC < 39.9:
+    print(f"{nome} está acima do peso com obesidade grau 1")
+elif calculoIMC > 40:
+    print(f"{nome} está acima do peso com obesidade grau 2")
